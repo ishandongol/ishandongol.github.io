1c174fb115a3dfb69f63a964feb932a11e13b94d
ishandongol <ishandongol@gmail.com>
2020-09-30 11:51:10 +0000

    [DESIGN FIXES]

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,13 +1,20 @@
+import Skills from "./skills"
 
 const Intro = () => {
   return (
-    <section className="h-calculated">
-      <div className="bg-hero bg-no-repeat h-calculated bg-bottom md:ml-auto md:w-7/12">
-
+    <section className="h-calculated md:grid lg:grid-cols-3 gap-4">
+      <div className="lg:col-span-1 h-auto lg:h-calculated">
+    <h2 className="text-center lg:text-left text-xl md:text-6xl font-bold text-gray-700 tracking-tighter leading-tight py-6">
+        <span className="text-sajilo-yellow mr-2 lg:mr-4">{'{'}</span><span className="text-gray-500 mr-2 lg:mr-4">{'...'}</span>{'ईशान डंगोल'}<span className="text-sajilo-yellow ml-2 md:ml-4">{'}'}</span>
+      </h2>
+      <p className="text-center lg:text-left text-xs md:text-sm font-bold text-gray-700 tracking-tighter leading-tight pb-2 pt-2">
+        <span className="text-sajilo-yellow mr-1 lg:mr-2">{'['}</span>Computer Engineer<span className="text-gray-500 mr-1 lg:mr-2">{','}</span>JavaScript Developer<span className="text-sajilo-yellow ml-2 md:ml-4">{']'}</span>
+      </p>
+      <Skills/>
       </div>
-      {/* <h2 className="text-md md:text-l6 lg:text-4xl font-bold text-gray-700 tracking-tighter leading-tight pb-6 md:pb-12 lg:pb-24">
-        <span className="text-sajilo-yellow mr-2 md:mr-4">{'{'}</span><span className="text-gray-500 mr-2 md:mr-4">{'...'}</span>{'ईशान डंगोल'}<span className="text-sajilo-yellow ml-2 md:ml-4">{'}'}</span>
-      </h2> */}
+      <div className="lg:col-span-2 bg-hero bg-no-repeat lg:bg-right-bottom lg:h-calculated h-64 bg-center ">
+      </div>
+      
     </section>
   )
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div className="mb-12 lg:mb-0 lg:inline-flex text-center">
               {skills.map((skill) => {
                 return (
-                  <i className={`flex-1 hover:text-sajilo text-gray-700 text-center pl-2 pr-2 pt-4 lg:pt-6 lg:pl-0 lg:pr-4 text-3xl lg:text-6xl lab la-${skill.name}`}></i>
+                  <i className={`flex-1 hover:text-sajilo-yellow text-gray-700 text-center pl-2 pr-2 pt-4 lg:pt-6 lg:pl-0 lg:pr-4 text-3xl lg:text-6xl lab la-${skill.name}`}></i>
                 )
               })}
             </div>
