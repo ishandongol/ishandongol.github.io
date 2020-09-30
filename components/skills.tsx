92d7d6a59866f646881b61f64f5972af425f6325
ishandongol <ishandongol@gmail.com>
2020-09-30 11:58:47 +0000

    [HOME PAGE LAYOUT]

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,11 @@
 
 import { skills } from '../lib/constants'
 
-const Skills = () => {
+const Skills = ({star}:{star:boolean}) => {
+  const filteredSkills = star ? skills.filter((skill) => skill.star) : skills
   return (
     <div className="mb-12 lg:mb-0 lg:inline-flex text-center">
-              {skills.map((skill) => {
+              {filteredSkills.map((skill) => {
                 return (
                   <i className={`flex-1 hover:text-sajilo-yellow text-gray-700 text-center pl-2 pr-2 pt-4 lg:pt-6 lg:pl-0 lg:pr-4 text-3xl lg:text-6xl lab la-${skill.name}`}></i>
                 )
