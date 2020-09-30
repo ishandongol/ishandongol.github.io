9da820b2842e08ab911df28064148e16ef1f2375
ishandongol <ishandongol@gmail.com>
2020-09-30 04:10:29 +0000

    [COLOR]

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@
 const Intro = () => {
   return (
     <section className="flex-row flex items-end md:justify-between h-64 lg:h-calculated hero w-full">
-      <h2 className="text-md md:text-l6 lg:text-4xl font-bold text-gray-800 tracking-tighter leading-tight pb-6 md:pb-12 lg:pb-24">
+      <h2 className="text-md md:text-l6 lg:text-4xl font-bold text-gray-700 tracking-tighter leading-tight pb-6 md:pb-12 lg:pb-24">
   <span className="text-sajilo-yellow mr-3">{'{'}</span><span className="text-gray-500 mr-3">{'...'}</span>{'ईशान डंगोल'}<span className="text-sajilo-yellow ml-3">{'}'}</span>
       </h2>
     </section>
