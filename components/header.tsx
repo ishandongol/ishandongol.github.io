00a353ef6ccf052ee04cc0c8e043aa5a2694bd27
ishandongol <ishandongol@gmail.com>
2020-10-04 07:55:28 +0000

    [DESIGN FIXES]

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Container from '../components/container'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import HeaderLogo from './header-logo'
 
 interface HeaderProps {
   className?: string
@@ -33,33 +34,36 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
     <nav className={`z-10 flex items-center font-bold justify-between flex-wrap p-6 ${className || ''} sticky top-0 bg-white`}>
       <Container>
-        {/* <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link href="/">
-          <a className="hover:underline h-8 subpixel-antialiased ">{'< ईशान डंगोल />'}</a>
-        </Link>
-      </div> */}
-        <div className="block lg:hidden ">
+        
+        <div className="flex lg:hidden">
+        <div className="flex-auto text-left py-1">
+          <HeaderLogo/>
+          </div>
           <button onClick={()=>{
             setToggled(!toggled)
-          }} className="flex items-center px-3 py-2 border rounded text-gray-700 border-sajilo hover:text-gray-800 hover:border-gray-800">
+          }} className="flex items-center px-3 py-2 border rounded text-gray-700 border-sajilo-yellow hover:text-gray-800 hover:border-gray-800 font-bold">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
           </button>
+          
         </div>
-        <div className="block flex-grow lg:flex lg:items-center lg:w-auto lg:text-right">
+        <div className="inline flex-grow lg:flex lg:items-center lg:w-auto lg:text-right">
+        <div className="lg:inline-block lg:align-middle hidden">
+        <HeaderLogo/>
+        </div>
           <div className={`text-sm lg:flex-grow ${toggled ? '':'hidden'} lg:inline`}>
             {headerItems.map((header,index) => {
               const { dynamic,url } = header;
               if(dynamic){
                 return (
-                  <HeaderLink key={url+index} {...header} className={router.asPath.includes(url) ? 'text-gray-900 border-b-4 border-sajilo-yellow' : 'text-gray-700'} />
+                  <HeaderLink key={url+index} {...header} className={router.asPath.includes(url) ? 'text-gray-900 border-b-2 py-1 border-sajilo-yellow' : 'text-gray-700'} />
                 )
               }
               return (
-                <HeaderLink  key={url+index} {...header} className={router.asPath === url ? 'text-gray-900 border-b-4 border-sajilo-yellow':'text-gray-700'} />
+                <HeaderLink  key={url+index} {...header} className={router.asPath === url ? 'text-gray-900 border-b-2 py-1 border-sajilo-yellow':'text-gray-700'} />
               )
             })}
             <Link href="/contact">
-              <a className={`inline-block text-sm px-4 py-2 leading-none border border-sajilo-yellow rounded ${router.asPath === '/contact' ? ' bg-sajilo-yellow text-sajilo' : 'text-gray-700'} hover:border-sajilo-yellow hover:text-sajilo hover:bg-sajilo-yellow mt-4 lg:mt-0`}>Contact</a>
+              <a className={`inline-block text-sm px-4 py-2 leading-none border-2 border-sajilo-yellow rounded ${router.asPath === '/contact' ? ' bg-sajilo-yellow text-sajilo' : 'text-gray-700'} hover:border-sajilo-yellow hover:text-sajilo hover:bg-sajilo-yellow mt-4 lg:mt-0`}>Contact</a>
             </Link>
           </div>
         </div>
