c6d8ba587838387fceaef3ef3f959b57acff6510
ishandongol <ishandongol@gmail.com>
2020-10-04 07:58:08 +0000

    [FOOTER DESIGN]

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <div className=" lg:w-3/4">
-          <div className="font-bold text-center lg:text-left text-xl mb-5">
+          <div className="font-bold text-center lg:text-left text-2xl lg:text-xl mb-12 lg:mb-5">
           <HeaderLogo/>
           </div>
-            <h3 className="text-3xl lg:text-5xl font-bold tracking-tighter text-sajilo leading-tight text-center lg:text-left mb-2 lg:mb-0 lg:pr-4">
+            <h3 className="text-3xl lg:text-5xl font-bold tracking-tighter text-sajilo leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
               the full stack developer you would <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-10 w-10 inline-block text-sajilo-yellow">
                 <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
               </svg> to work with.
