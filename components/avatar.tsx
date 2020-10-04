6af0b58529aa44b59211216fc14b7c69735fc2bf
ishandongol <ishandongol@gmail.com>
2020-10-04 10:05:16 +0000

    [EMAIL]

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,13 +1,17 @@
 type Props = {
   name: string
   picture: string
+  email?: string
 }
 
-const Avatar = ({ name, picture }: Props) => {
+const Avatar = ({ name, picture,email }: Props) => {
   return (
     <div className="flex items-center">
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <div>
       <div className="text-xl font-bold">{name}</div>
+      <div className="text-sm font-sm text-gray-600">{email || 'ishandongol@gmail.com'}</div>
+      </div>
     </div>
   )
 }
