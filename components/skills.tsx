dc988106f19bdb464eed7996ffbdb5b87c9f9eac
ishandongol <ishandongol@gmail.com>
2020-10-04 04:17:47 +0000

    [HOME PAGE DESIGN]

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,16 +1,16 @@
 
 import { skills } from '../lib/constants'
 
-const Skills = ({star}:{star?:boolean}) => {
+const Skills = ({ star, containerClassName, iconClassName }: { star?: boolean, containerClassName?: string, iconClassName?: string }) => {
   const filteredSkills = star ? skills.filter((skill) => skill.star) : skills
   return (
-    <div className="mb-12 lg:mb-0 lg:inline-flex text-center">
-              {filteredSkills.map((skill) => {
-                return (
-                  <i className={`flex-1 hover:text-sajilo-yellow text-gray-700 text-center pl-2 pr-2 pt-4 lg:pt-6 lg:pl-0 lg:pr-4 text-3xl lg:text-6xl lab la-${skill.name}`}></i>
-                )
-              })}
-            </div>
+    <div className={`${containerClassName || 'mb-12 lg:mb-0 lg:inline-flex text-center'}`}>
+      {filteredSkills.map((skill) => {
+        return (
+          <i className={`${iconClassName || 'flex-1 hover:text-sajilo-yellow text-gray-700 text-center pl-2 pr-2 pt-4 lg:pt-6 lg:pl-0 lg:pr-4 text-3xl lg:text-6xl'} lab la-${skill.name}`}></i>
+        )
+      })}
+    </div>
   )
 }
 
