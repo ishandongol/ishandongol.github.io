30298b120570053f9b3fda135464aec157bc84b0
ishandongol <ishandongol@gmail.com>
2020-10-04 13:14:05 +0000

    [EMAIL LINK AVATAR]

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -10,7 +10,9 @@ const Avatar = ({ name, picture,email }: Props) => {
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div>
       <div className="text-xl font-bold">{name}</div>
-      <div className="text-sm font-sm text-gray-600">{email || 'ishandongol@gmail.com'}</div>
+      <div className="text-sm font-sm text-gray-600">
+      <a target="_blank"  rel="noreferrer noopener" href={`mailto:${email || 'ishandongol@gmail.com'}?subject=Contact%20|%20Portfolio%20|%20Blog`}>{email || 'ishandongol@gmail.com'}</a>
+      </div>
       </div>
     </div>
   )
