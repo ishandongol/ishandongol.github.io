313b882ac163241e3f16eb919cbab0f2bb36cf8b
Ishan Dongol <ishandongol@gmail.com>
2021-09-05 17:04:31 +0545

    Updates

---
+++ _next/static/wVA9tJGKkPSTnqwfq4WS-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fexperience","\u002Fportfolio","\u002Fportfolio\u002F[slug]\u002Fprivacy","\u002Fportfolio\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
