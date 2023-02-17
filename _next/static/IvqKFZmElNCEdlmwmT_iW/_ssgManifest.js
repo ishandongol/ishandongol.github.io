45b6eba272b8bdeec035398e60db3cc8d31671b0
ishandongol <>
2023-02-17 07:27:49 +0000

    Update from https://github.com/ishandongol/portfolio-2023/commit/70b5b58f8630ee5d06d688a1e86854ae393fd1c3

---
+++ _next/static/IvqKFZmElNCEdlmwmT_iW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fcontact","\u002Fportfolio","\u002Fportfolio\u002F[slug]","\u002Fportfolio\u002F[slug]\u002Fprivacy"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
